057fbfc2dc16d50bb90659f0e8b1e6027c7dec76
aatmaani <111965953+aatmaani@users.noreply.github.com>
2022-09-06 15:26:46 +0530

    Update Dev.sh

--- Dev.sh
+++ Dev.sh
@@ -6,7 +6,7 @@ cd Dev-v1
 git clone https://github.com/aatmaani/project.git
 
 cd project
-docker build -t project/nodejs-app:Dev-v1
+docker build -t project/nodejs-app:Dev-v1 .
 
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 215767047920.dkr.ecr.ap-south-1.amazonaws.com
 #sudo  docker tag 123 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:v1 
